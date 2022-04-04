c9a55311fd00739285ab8040636c03bdf323c6d3
Krekhov Nikolai <krekhov.na@phystech.edu>
2022-04-04 21:36:20 +0300

    fix bag with down array

--- Game/game.py
+++ Game/game.py
@@ -209,7 +209,7 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w or event.key == pygame.K_UP:
                 menu.switch(-1)
-            elif event.key == pygame.K_s or event.key == pygame.KEYDOWN:
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
                 menu.switch(1)
             elif event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
                 menu.select()
